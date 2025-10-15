e091a4f1bfd5735f7f97157052081991e296932b
Parth Chhabra <129220554+when-pig-fly@users.noreply.github.com>
2025-10-14 21:35:29 -0700

    PLS WORK

---
+++ components/Footer.tsx
@@ -0,0 +1,55 @@
+import { FaLocationArrow } from "react-icons/fa6";
+
+import { socialMedia } from "@/data";
+import MagicButton from "./MagicButton";
+
+const Footer = () => {
+  return (
+    <footer className="w-full pt-20 pb-10 relative" id="contact">
+      {/* background grid */}
+      <div className="w-full absolute left-0 -bottom-0 min-h-96 overflow-hidden">
+        <img
+          src="/footer-grid.svg"
+          alt="grid"
+          className="w-full h-full opacity-50 "
+        />
+      </div>
+
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw]">
+          Ready to change the <span className="text-purple">world</span> ?
+        </h1>
+        <p className="text-white-200 md:mt-10 my-5 text-center">
+          Reach out to me today
+        </p>
+        <a href="mailto:parth_chhabra@berkeley.edu">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((info) => (
+            <a
+              key={info.id}
+              href={info.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+            >
+              <img src={info.img} alt="icons" width={20} height={20} />
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,161 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Also interested in making bombs",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "absolute right-0 w-2/3 h-full object-contain opacity-80",
+    titleClassName: "justify-end",
+    img: "/bomb.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "New skills",
+    description: "I constantly try to learn",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Student with a passion for changing the world.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title: "Currently learning how to code",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Photo/Video Montage Creator",
+    des: "A creative tool for building photo and video montages.",
+    img: "/photo-video_montage.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://parthchhabra-wq.github.io/",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "McDonald's",
+    desc: "I spat in your food",
+    className: "md:col-span-2",
+    thumbnail: "/mcdonald.svg",
+  },
+  {
+    id: 2,
+    title: "Pawnshop",
+    desc: "Lowball old people",
+    className: "md:col-span-2",
+    thumbnail: "/pawnshop.svg",
+  },
+  {
+    id: 3,
+    title: "Car Salesman",
+    desc: "Used cars for new prices",
+    className: "md:col-span-2",
+    thumbnail: "/jaguar.png",
+  },
+  {
+    id: 4,
+    title: "Movie Theater Janitor",
+    desc: "Found $10",
+    className: "md:col-span-2",
+    thumbnail: "/popcorn.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/parthchhabra-wq",
+  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  //   link: "https://twitter.com/yourusername",
+  // },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://linkedin.com/in/parth-chhabra-32b847225/",
+  },
+];
